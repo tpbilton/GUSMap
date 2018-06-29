412554d446042bd84e6105873d4e90a9e2d721ce
Timothy Bilton (Home) <tpbilton@gmail.com>
2018-06-29 17:09:07 +1200

    Minor change somewhere

--- R/GUSMap-package.R
+++ R/GUSMap-package.R
@@ -1,5 +1,5 @@
 ##########################################################################
-# Genotyping Uncertainty with Sequencing data and linkage MAPping
+# Genotyping Uncertainty with Sequencing data (GUSbase)
 # Copyright 2017-2018 Timothy P. Bilton <tbilton@maths.otago.ac.nz>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -11,26 +11,27 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 #
-#' Genotyping Uncertainty with Sequencing data for linkage Mapping (GUSMap)
-#' 
-#' Package for constructing genetic maps using outcrossed full-sib family
-#' populations that have been sequenced using next generation multiplexing
-#' sequencing methods \insertCite{@see @bilton2018genetics1}{GUSMap}.
-#' 
-#' \tabular{ll}{ Package: \tab GUSMap\cr Type: \tab Package\cr Version: \tab
-#' 1.0.0\cr Date: \tab 2018-06-11\cr License: \tab GPL 3\cr }
-#' 
-#' @name GUSMap-package
-#' @aliases GUSMap
+#' Genotyping Uncertainty with Sequencing data (GUSbase)
+#'
+#' Package for analyzing high-throughput sequencing data generated using genotying-by-sequencing (GBS),
+#' exome capture sequencing, restriction site-associated DNA sequencing (RADseq) or whole genome sequencing (WGS).
+#' The methods for analyzing the sequencing data in this package are all based on using the number of reads for
+#' reference and alternate allele.
+#'
+#' \tabular{ll}{ Package: \tab GUSbase\cr Type: \tab Package\cr Version: \tab
+#' 0.1.0\cr Date: \tab 2018-06-23\cr License: \tab GPL 3\cr }
+#'
+#' @name GUSbase-package
+#' @aliases GUSbase
 #' @docType package
 #' @author Timothy P. Bilton, Maintainer: Timothy P. Bilton
 #' <tbilton@@maths.otago.ac.nz>
-#' @references 
+#' @references
 #' \insertAllCited{}
 #' @keywords package
 #' @importFrom Rdpack reprompt
