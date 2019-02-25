704daf964c86dc795bf7dd52e766d8f62a624aa4
Timothy P. Bilton (Home) <tpbilton@gmail.com>
2019-02-26 11:15:36 +1300

    Some minor changes

--- test/viterbi.cpp
+++ test/viterbi.cpp
@@ -1,7 +1,7 @@
 /*
 ##########################################################################
 # Genotyping Uncertainty with Sequencing data and linkage MAPping (GUSMap)
-# Copyright 2017 Timothy P. Bilton <tbilton@maths.otago.ac.nz>
+# Copyright 2017-2019 Timothy P. Bilton <tbilton@maths.otago.ac.nz>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
- */
+*/
 
 #include <math.h>
 #include <Rcpp.h>
