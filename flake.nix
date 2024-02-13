0e3babfc7a9f8934ed52b02b12c4f11d79ff2976
Simon Guest <simon.guest@agresearch.co.nz>
2024-02-14 09:11:29 +1300

    Example of installing an R package directly from GitHub

--- flake.nix
+++ flake.nix
@@ -22,6 +22,28 @@
             GUSbase = GUSbase.packages.${system}.default;
           };
 
+          # We call this myAGHmatrix, because AGHmatrix is already packaged in Nix,
+          # and we're only doing it like this for the purpose of having an example.
+          #
+          # The rev is obtained by copying the full SHA for the required commit on GitHub.
+          # The sha256 is obtained from that rev like this:
+          # nix-prefetch-url --unpack https://github.com/prmunoz/AGHmatrix/archive/73e7b743cd57b81957a1aec20126955717507241.tar.gz
+          #
+          # The propagated build inputs are copied from the Imports sections of the R package DESCRIPTION.
+          # If any of these are not packaged in Nix already, they must be made available like we are doing for myAGHmatrix.  Recursively.
+          myAGHmatrix = with pkgs;
+            rPackages.buildRPackage {
+              name = "AGHmatrix";
+              src = fetchFromGitHub {
+                owner = "prmunoz";
+                repo = "AGHmatrix";
+                rev = "73e7b743cd57b81957a1aec20126955717507241";
+                sha256 = "0whfkflxjxp3qgf4332scz4b3yrvb7v3j0x9iy3vhbl4fxmdapdj";
+              };
+              propagatedBuildInputs = with rPackages;
+                [ Matrix zoo ];
+            };
+
           GUSMap = with pkgs;
             rPackages.buildRPackage {
               name = "GUSMap";
@@ -33,7 +55,7 @@
           R-with-GUSMap = with pkgs;
             rWrapper.override {
               packages = with rPackages;
-                [ GUSMap ];
+                [ GUSMap myAGHmatrix ];
             };
         in
           with pkgs;
