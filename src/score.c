3efef786c293cab5ed8fcf9f08f2f7632af0afec
Timothy Bilton <tpbilton@users.noreply.github.com>
2026-09-17 20:45:04 +1200

    Update score.c
    fix for mac install

--- src/score.c
+++ src/score.c
@@ -1,7 +1,7 @@
 /*
 ##########################################################################
 # Genotyping Uncertainty with Sequencing data and linkage MAPping (GUSMap)
-# Copyright 2017-2019 Timothy P. Bilton <tbilton@maths.otago.ac.nz>
+# Copyright 2017-2026 Timothy P. Bilton <timothy.bilton@agresearch.co.nz>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,15 +17,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
  */
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
 
 #include <R.h>
 #include <Rinternals.h>
 #include <Rmath.h>
 #include <math.h>
 #include "probFun.h"
+
 #ifdef _OPENMP
-    #include <omp.h>
-#else
     inline int omp_get_max_threads() { return 1; }
 #endif
 
